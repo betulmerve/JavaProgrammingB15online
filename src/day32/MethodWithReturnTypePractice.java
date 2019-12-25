c8aaadf3534956a38703433c9c6a56b5706aa826
betulmerve <mervefaki@gmail.com>
2019-12-24 21:42:21 -0500

    second time

---
+++ src/day32/MethodWithReturnTypePractice.java
@@ -0,0 +1,18 @@
+package day32;
+
+public class MethodWithReturnTypePractice {
+
+    public static void main(String[] args) {
+
+        System.out.println( divide(10,3));
+
+    }
+
+    //create a method called divide
+    //it has 2 double parameters and return the value of result as double
+
+    public static double divide(double num1,double num2) {
+
+        return num1/num2;
+    }
+}
