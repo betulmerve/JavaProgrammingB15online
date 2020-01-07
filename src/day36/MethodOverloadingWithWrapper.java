16693df7c550d649d174eb9ca611714607a99c93
betulmerve <mervefaki@gmail.com>
2020-01-07 17:51:42 -0500

    day37

---
+++ src/day36/MethodOverloadingWithWrapper.java
@@ -0,0 +1,28 @@
+package day36;
+
+public class MethodOverloadingWithWrapper {
+
+    public static void main(String[] args) {
+
+
+        printNum(3);
+        printNum(new Integer(4));
+    }
+
+    //it is always looking for exact match!
+    //if it is found it is found
+    //if it is not found it will try to look for something else
+
+
+    public static void printNum(int x) {
+
+        System.out.println("Printing primitive x= "+x);
+
+    }
+
+    public static void printNum(Integer x) {
+
+        System.out.println("Printing Wrapper Object x= "+x);
+
+    }
+}
--- src/day37/ArrayListRemoveItemPractice.java
+++ src/day37/ArrayListRemoveItemPractice.java
@@ -1,8 +1,29 @@
 package day37;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListRemoveItemPractice {
 
     public static void main(String[] args) {
+
+        List<Integer> lst = new ArrayList<>();
+        lst.add(2);
+        lst.add(14);
+        lst.add(34);
+        lst.add(41);
+        lst.add(19);
+        System.out.println("lst = " + lst);
+
+        //bu index i remove ediyor
+        lst.remove(3);
+        System.out.println("lst = " + lst);
+        lst.remove(2);
+        System.out.println("lst = " + lst);
+
+        //bu value remove ediyor
+        lst.remove(new Integer(2));
+        System.out.println("lst = " + lst);
 
     }
 }
