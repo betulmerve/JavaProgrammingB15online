1bb1e857b117e8716227fcfc585f57185730347d
betulmerve <mervefaki@gmail.com>
2020-01-16 21:37:30 -0500

    day42

--- src/day42/Bike.java
+++ src/day42/Bike.java
@@ -5,5 +5,25 @@ public class Bike {
     int speed;
     int gear;
 
+    // constructor is a block of code
+    // that has same name as classname
+    // and has no return type
+    // if it does not meet above requirement
+    // it will not be a constructor
+    // the only time you can call this is :
+    // when object is being created
+
+    public Bike() {
+        gear=1;
+    }
+
+    public Bike(int gear) {
+        this.gear=gear;
+    }
+
+    public Bike(int gear, int speed) {
+        this.gear=gear;
+        this.speed=speed;
+    }
 
 }
--- src/day42/BikeAction.java
+++ src/day42/BikeAction.java
@@ -4,6 +4,27 @@ public class BikeAction {
 
     public static void main(String[] args) {
 
+        Bike b1=new Bike();
+        Bike b2=new Bike();
+
+        // i don't want empty gear when object is created
+        // i want to the bike come with gear set to 1
+
+        System.out.println("b1 gear = " + b1.gear);
+        System.out.println("b2 gear = " + b2.gear);
+
+        // I want to be able to set the default gear to
+        // any number i specify when object is being created
+
+        Bike b3=new Bike(3);
+        System.out.println("b3.gear = " + b3.gear);
+
+        Bike b4=new Bike(2,25);
+        System.out.println("b4.gear = " + b4.gear);
+        System.out.println("b4.speed = " + b4.speed);
+
+
+
 
 
     }
