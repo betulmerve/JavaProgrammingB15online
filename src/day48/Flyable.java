be8635b082e0e5d84c963c16a0428036ae33295b
betulmerve <mervefaki@gmail.com>
2020-01-28 15:52:29 -0500

    day48

--- src/day48/Flyable.java
+++ src/day48/Flyable.java
@@ -1,6 +1,25 @@
 package day48;
 
+// this is how we create an interface
+// interface is abstract type just like abstract class
+// no object can be created from it
+// it provide guideline for implementing classes
+// it's like signing a contract
 public interface Flyable {
 
+    //an interface public static final field
+    public static final boolean HAVE_WING=true;
 
+    public abstract void fly();
+
+    // can we have constructor ? : ABSOLUTELY NO !!!!!!!!!!!
+
+
+
+
+    //abstract class olusturdugumuzda sub classlarin ortak bir cok ozellikleri oluyor
+    //instance field olusturarak bunlari destekliyoruz
+    //fakat interface sub classlarinin tek bir ortak ozelligi var genelde
+    // ornegin flyable interface altinda kite, superman, plane :tek ozellik ucabilmeleri
+    //
 }
