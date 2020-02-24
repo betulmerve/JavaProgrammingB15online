a91f5edc42a9d85ba993dc5fc9cc65c60c803079
betulmerve <mervefaki@gmail.com>
2020-02-23 19:50:37 -0500

    day63

--- src/day63/ReadingFilePractice.java
+++ src/day63/ReadingFilePractice.java
@@ -14,11 +14,15 @@ public class ReadingFilePractice {
         List<String> allData=Files.readAllLines(Paths.get("src/day63/employeeData.txt"));
         Map<Integer,String > idNamePair=new HashMap<>();
         for (String eachLine:allData) {
+//            int id=Integer.parseInt(eachLine.split(",")[0]);
+//            String name=eachLine.split(",")[1];
             String[] strArr=eachLine.split(",");
             idNamePair.put(Integer.parseInt(strArr[0]),strArr[1]);
         }
 
         System.out.println("idNamePair = " + idNamePair);
+        // get the person with employee id with number 20
+        System.out.println("idNamePair.get(20) = " + idNamePair.get(20));
 
 //        allData.forEach(eachLine-> System.out.println("eachLine = " + eachLine));
 
